e89fb61387ccf59bc20d9507110f0179a192df83
Đinh Thị Thanh Bình <thanhbinh57kt@mac-thanh-binh.local>
2024-03-16 16:08:46 +0700

    feat:(data) add IMAGES

---
+++ src/data/images.ts
@@ -0,0 +1,42 @@
+export const IMAGES = [
+  {
+    name: "Tom",
+    src: "./tom.webp",
+    id: 1,
+  },
+  {
+    name: "Chaien",
+    src: "./chaien.jpeg",
+    id: 2,
+  },
+  {
+    name: "Doreamon",
+    src: "./doreamon.webp",
+    id: 3,
+  },
+  {
+    name: "Nobita",
+    src: "./nobita.jpeg",
+    id: 4,
+  },
+  {
+    name: "Xeko",
+    src: "./xeko.jpeg",
+    id: 5,
+  },
+  {
+    name: "Xuka",
+    src: "./xuka.jpeg",
+    id: 6,
+  },
+  {
+    name: "Jerry",
+    src: "./jerry.webp",
+    id: 7,
+  },
+  {
+    name: "Chopper",
+    src: "./chopper.jpeg",
+    id: 8,
+  },
+];
